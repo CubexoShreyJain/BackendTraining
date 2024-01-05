5c1a70cf688082d0947e87f9408ba5e5a8024ca6
Shrey Jain <shrey.jain@cubexo.io>
2024-01-05 15:23:27 +0530

    Python training- day1-2-3

---
+++ day1-python/ConditionalStatement.py
@@ -0,0 +1,7 @@
+statement = input("Enter statement: ")
+
+if "Shrey" in statement:
+    print("Shrey Present")
+else:
+    print("Shrey Absent")
+
---
+++ day2-python/generator.py
@@ -0,0 +1,6 @@
+#Print even numbers:
+res = (x for x in range(1, 101) if x%2 ==0)
+for i in res:
+    
+    
+    print(i)
